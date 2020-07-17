1bec89a951f85de7cad97d701752487b892aa10d
tjgillweb <tjgillweb@gmail.com>
2020-07-17 16:22:36 +0530

    import and call quizService API and render questions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './assets/style.css';
+import quizService from './quizService'
 
 class QuizBee extends React.Component {
+    state = {
+        questionBank : []
+    };
+    getQuestions = () => {
+        quizService().then(question => {
+            this.setState({
+                questionBank: question
+            })
+        })
+    }
+
+    componentDidMount() {
+        this.getQuestions()
+    }
+
     render(){
         return (
             <div className="container">
                 <div className="title">QuizBee</div>
+                    {
+                        this.state.questionBank.length > 0 &&
+                        this.state.questionBank.map(({question, answers, correct, questionId}) => 
+                        <h4>{question}</h4>)
+                    }
             </div>
         )
     }
