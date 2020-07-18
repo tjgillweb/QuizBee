8ae5e20a3b29612821dba2b78d2d7b7494c82732
tjgillweb <tjgillweb@gmail.com>
2020-07-18 14:52:43 +0530

    add build files

---
+++ precache-manifest.ee8fd517433dd69295e703180e08cf04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bbcfba21d618898901180392a41a638",
+    "url": "/quizbee/index.html"
+  },
+  {
+    "revision": "696b3eb9b1dc91ed781c",
+    "url": "/quizbee/static/css/main.0e246d08.chunk.css"
+  },
+  {
+    "revision": "431e44eca0f0169fbbc7",
+    "url": "/quizbee/static/js/2.9f25e938.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quizbee/static/js/2.9f25e938.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "696b3eb9b1dc91ed781c",
+    "url": "/quizbee/static/js/main.49b034ba.chunk.js"
+  },
+  {
+    "revision": "1192c94079eb9535dd92",
+    "url": "/quizbee/static/js/runtime-main.5307379a.js"
+  }
+]);
